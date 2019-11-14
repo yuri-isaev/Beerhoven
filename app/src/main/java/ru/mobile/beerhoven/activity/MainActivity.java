4096a9042d47cd96a06a0a58902b689246b5041a
Yurii-Isaev <devhouse27@gmail.com>
2019-11-14 21:16:02 +0200

    Initial commit

---
+++ app/src/main/java/ru/mobile/beerhoven/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package ru.mobile.beerhoven.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import ru.mobile.beerhoven.R;
+
+public class MainActivity extends AppCompatActivity {
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+   }
+}
