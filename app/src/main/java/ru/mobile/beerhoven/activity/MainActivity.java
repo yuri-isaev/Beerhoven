e3fe373196b9e8ad98fb7f859d3dd1a55215b575
Yurii-Isaev <devhouse27@gmail.com>
2019-11-19 19:12:38 +0200

    feat: navigation drawer font

--- app/src/main/java/ru/mobile/beerhoven/activity/MainActivity.java
+++ app/src/main/java/ru/mobile/beerhoven/activity/MainActivity.java
@@ -1,14 +1,22 @@
 package ru.mobile.beerhoven.activity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.ui.AppBarConfiguration;
 
 import com.google.android.material.navigation.NavigationView;
 
 import ru.mobile.beerhoven.R;
+import ru.mobile.beerhoven.custom.CustomTypeFaceSpan;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,5 +34,31 @@ public class MainActivity extends AppCompatActivity {
       mAppBarConfiguration = new AppBarConfiguration
           .Builder(R.id.nav_news, R.id.nav_store, R.id.nav_order, R.id.nav_cart, R.id.nav_map)
           .setDrawerLayout(drawer).build();
+
+      changeFont();
+   }
+
+   private void changeFont() {
+      Menu menu = mNavigationView.getMenu();
+      for (int i = 0; i < menu.size(); i++) {
+         MenuItem menuItem = menu.getItem(i);
+         // For applying a font to subMenu.
+         SubMenu subMenu = menuItem.getSubMenu();
+         if (subMenu != null && subMenu.size() > 0) {
+            for (int j = 0; j < subMenu.size(); j++) {
+               MenuItem subMenuItem = subMenu.getItem(j);
+               applyFontToMenuItem(subMenuItem);
+            }
+         }
+         applyFontToMenuItem(menuItem);
+      }
+   }
+
+   private void applyFontToMenuItem(MenuItem mi) {
+      Typeface font = ResourcesCompat.getFont(this, R.font.catorze27_style_1);
+      SpannableString mNewTitle = new SpannableString(mi.getTitle());
+      mNewTitle.setSpan(new CustomTypeFaceSpan("", font), 0, mNewTitle.length(),
+          Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+      mi.setTitle(mNewTitle);
    }
 }
