209c1825634f99bef7601f038eaddaa4d15ed1a5
Yurii-Isaev <devhouse27@gmail.com>
2019-12-26 20:56:26 +0200

    feat: catalog card popup menu

--- app/src/main/java/ru/mobile/beerhoven/ui/store/catalog/CatalogAdapter.java
+++ app/src/main/java/ru/mobile/beerhoven/ui/store/catalog/CatalogAdapter.java
@@ -1,26 +1,36 @@
 package ru.mobile.beerhoven.ui.store.catalog;
 
+import static android.widget.PopupMenu.*;
+
+import static ru.mobile.beerhoven.ui.store.catalog.CatalogAdapter.*;
+
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.PopupMenu;
 
 import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.RecyclerView.Adapter;
+import androidx.recyclerview.widget.RecyclerView.ViewHolder;
 
 import java.util.List;
 
 import ru.mobile.beerhoven.R;
+import ru.mobile.beerhoven.databinding.ItemCatalogBinding;
 import ru.mobile.beerhoven.interfaces.InteractionListener;
 import ru.mobile.beerhoven.models.Item;
+import ru.mobile.beerhoven.utils.HashMapRepository;
 
-public class CatalogAdapter extends RecyclerView.Adapter<CatalogAdapter.ItemHolder> {
-
+public class CatalogAdapter extends Adapter<ItemHolder> implements OnMenuItemClickListener {
    protected List<Item> mAdapterList;
    private final InteractionListener mListener;
+   private NavController mNavController;
 
    public CatalogAdapter(List<Item> list, Context context, InteractionListener mListener) {
       this.mAdapterList = list;
@@ -30,18 +40,44 @@ public class CatalogAdapter extends RecyclerView.Adapter<CatalogAdapter.ItemHold
    @NonNull
    @Override
    public ItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-      View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_catalog, parent, false);
-      return new ItemHolder(view);
+      ItemCatalogBinding recyclerBinding = ItemCatalogBinding
+          .inflate(LayoutInflater.from(parent.getContext()), parent, false);
+      return new ItemHolder(recyclerBinding);
    }
 
    @SuppressLint({"NonConstantResourceId", "SetTextI18n"})
    @Override
    public void onBindViewHolder(@NonNull ItemHolder holder, int position) {
       Item model = mAdapterList.get(position);
-      holder.mItemTitle.setText(model.getTitle());
-      holder.mItemPrice.setText(model.getPrice() + " руб.");
-      holder.mItemStyle.setText(model.getStyle());
-      holder.mItemFortress.setText(model.getFortress() + "%");
+      String PID = model.getId();
+      String image = model.getUrl();
+
+      holder.recyclerBinding.itemName.setText(model.getTitle());
+      holder.recyclerBinding.itemPrice.setText((model.getPrice() + " руб."));
+      holder.recyclerBinding.itemStyle.setText(model.getStyle());
+      holder.recyclerBinding.itemFortress.setText((model.getFortress() + "%"));
+      holder.recyclerBinding.itemContainer.setOnClickListener(v -> {
+         //
+      });
+      holder.recyclerBinding.itemSelector.setOnClickListener(v -> {
+         PopupMenu popupMenu = new PopupMenu(v.getContext(), v);
+         popupMenu.inflate(R.menu.popup_menu);
+         popupMenu.show();
+
+         popupMenu.setOnMenuItemClickListener(item -> {
+            switch (item.getItemId()) {
+               case R.id.action_context_open:
+               //
+                  break;
+               case R.id.action_context_delete:
+                  HashMapRepository.map.put("item_id", PID);
+                  HashMapRepository.map.put("image", image);
+                  mListener.onInteractionDelete(mAdapterList.get(position));
+                  break;
+            }
+            return true;
+         });
+      });
    }
 
    @Override
@@ -49,20 +85,29 @@ public class CatalogAdapter extends RecyclerView.Adapter<CatalogAdapter.ItemHold
       return mAdapterList.size();
    }
 
-   public static class ItemHolder extends RecyclerView.ViewHolder {
-      private final LinearLayout mItemContainer;
-      private final TextView mItemTitle;
-      private final TextView mItemPrice;
-      private final TextView mItemStyle;
-      private final TextView mItemFortress;
+   @Override
+   public boolean onMenuItemClick(MenuItem menuItem) {
+      return false;
+   }
 
-      public ItemHolder(@NonNull View itemView) {
-         super(itemView);
-         this.mItemContainer = itemView.findViewById(R.id.item_container);
-         this.mItemTitle = itemView.findViewById(R.id.item_title);
-         this.mItemPrice = itemView.findViewById(R.id.item_price);
-         this.mItemStyle = itemView.findViewById(R.id.item_style);
-         this.mItemFortress = itemView.findViewById(R.id.item_fortress);
+   public static class ItemHolder extends ViewHolder implements OnClickListener, OnMenuItemClickListener {
+      ItemCatalogBinding recyclerBinding;
+
+      public ItemHolder(ItemCatalogBinding recyclerBinding) {
+         super(recyclerBinding.getRoot());
+         this.recyclerBinding = recyclerBinding;
+      }
+
+      @Override
+      public void onClick(View v) {
+         recyclerBinding.itemSelector.setOnClickListener(this);
+         recyclerBinding.itemContainer.setOnClickListener(this);
+         recyclerBinding.itemPlus.setOnClickListener(this);
+      }
+
+      @Override
+      public boolean onMenuItemClick(MenuItem item) {
+         return false;
       }
    }
 }
--- app/src/main/java/ru/mobile/beerhoven/utils/HashMapRepository.java
+++ app/src/main/java/ru/mobile/beerhoven/utils/HashMapRepository.java
@@ -3,6 +3,7 @@ package ru.mobile.beerhoven.utils;
 import java.util.HashMap;
 
 public class HashMapRepository {
+   public static HashMap<String, String> map = new HashMap<>();
    public static HashMap<String, String> idMap = new HashMap<>();
    public static HashMap<String, Double> priceMap = new HashMap<>();
    public static HashMap<String, String> catalogMap = new HashMap<>();
