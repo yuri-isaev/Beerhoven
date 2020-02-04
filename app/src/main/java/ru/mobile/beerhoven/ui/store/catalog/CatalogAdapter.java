3815fee246782182fb344c52a3888ff51d6cb639
Yurii-Isaev <devhouse27@gmail.com>
2020-02-04 18:03:12 +0200

    feat: add product to order cart

--- app/src/main/java/ru/mobile/beerhoven/ui/store/catalog/CatalogAdapter.java
+++ app/src/main/java/ru/mobile/beerhoven/ui/store/catalog/CatalogAdapter.java
@@ -6,6 +6,7 @@ import static ru.mobile.beerhoven.ui.store.catalog.CatalogAdapter.*;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.icu.text.SimpleDateFormat;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,9 +22,12 @@ import androidx.recyclerview.widget.RecyclerView.ViewHolder;
 
 import com.bumptech.glide.Glide;
 
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
 
 import ru.mobile.beerhoven.R;
+import ru.mobile.beerhoven.activity.MainActivity;
 import ru.mobile.beerhoven.databinding.ItemCatalogBinding;
 import ru.mobile.beerhoven.domain.model.Product;
 import ru.mobile.beerhoven.interfaces.InteractionListener;
@@ -34,11 +38,13 @@ import ru.mobile.beerhoven.utils.HashMapRepository;
 public class CatalogAdapter extends Adapter<ItemViewHolder> implements OnMenuItemClickListener {
    protected List<Product> mAdapterList;
    private final InteractionListener mListener;
+   private final Context mContext;
    private NavController navController;
 
    public CatalogAdapter(List<Product> list, Context context, InteractionListener mListener) {
       this.mAdapterList = list;
       this.mListener = mListener;
+      this.mContext = context;
    }
 
    @NonNull
@@ -72,14 +78,19 @@ public class CatalogAdapter extends Adapter<ItemViewHolder> implements OnMenuIte
           .setDescription(model.getDescription())
           .setImage(image);
 
+      // Binding view fields
       holder.recyclerBinding.itemName.setText(model.getName());
       holder.recyclerBinding.itemPrice.setText((model.getPrice() + " руб."));
       holder.recyclerBinding.itemStyle.setText(model.getStyle());
       holder.recyclerBinding.itemFortress.setText((model.getFortress() + "%"));
+
+      // Navigate action for click product catalog card
       holder.recyclerBinding.itemContainer.setOnClickListener(v -> {
          navController = Navigation.findNavController(v);
          navController.navigate(action);
       });
+
+      // Navigate action for click product catalog card
       holder.recyclerBinding.itemSelector.setOnClickListener(v -> {
          PopupMenu popupMenu = new PopupMenu(v.getContext(), v);
          popupMenu.inflate(R.menu.popup_menu);
@@ -99,6 +110,48 @@ public class CatalogAdapter extends Adapter<ItemViewHolder> implements OnMenuIte
             }
             return true;
          });
+      });
+
+      // Add product to cart when click catalog card element
+      holder.recyclerBinding.itemPlus.setOnClickListener(v -> {
+         int mValue = 1;
+         Calendar calForDate = Calendar.getInstance();
+
+         @SuppressLint({"NewApi", "LocalSuppress", "SimpleDateFormat"})
+         SimpleDateFormat currentDate = new SimpleDateFormat(Constants.CURRENT_DATA);
+
+         @SuppressLint({"NewApi", "LocalSuppress"})
+         String saveCurrentDate = currentDate.format(calForDate.getTime());
+
+         @SuppressLint({"SimpleDateFormat", "NewApi", "LocalSuppress"})
+         SimpleDateFormat currentTime = new SimpleDateFormat(Constants.CURRENT_TIME);
+
+         @SuppressLint({"NewApi", "LocalSuppress"})
+         String saveCurrentTime = currentTime.format(calForDate.getTime());
+
+         HashMapRepository.idMap.put("catalog_id", model.getId());
+         HashMapRepository.priceMap.put("catalog_total", model.getPrice());
+         HashMapRepository.priceMap.put("catalog_price", model.getPrice());
+
+         HashMap<String, String> map = HashMapRepository.catalogMap;
+         map.put("name", model.getName());
+         map.put("country", model.getCountry());
+         map.put("manufacture", model.getManufacture());
+         map.put("style", model.getStyle());
+         map.put("fortress", model.getFortress());
+         map.put("density", model.getDensity());
+         map.put("description", model.getDescription());
+         map.put("url", model.getUrl());
+         map.put("data", saveCurrentDate);
+         map.put("time", saveCurrentTime);
+         map.put("quantity", String.valueOf(mValue));
+
+         mListener.onInteractionAdd(mAdapterList.get(position));
+
+         // Access to the activation method through the Adapter
+         if (mContext instanceof MainActivity) {
+            ((MainActivity) mContext).onIncreaseCounterClick();
+         }
       });
    }
 
